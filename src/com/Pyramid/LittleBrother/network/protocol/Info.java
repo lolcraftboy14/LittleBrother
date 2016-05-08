35e493fc571cf04230ceb1ee2e2d27fc22362860
lolcraftboy14 <lolcraftboy13.gamer@gmail.com>
2016-05-08 09:29:30 +0200

    14.2

--- src/com/Pyramid/LittleBrother/network/protocol/Info.java
+++ src/com/Pyramid/LittleBrother/network/protocol/Info.java
@@ -5,63 +5,69 @@ interface Info {
 	/**
 	 * Actual Minecraft: PE protocol version；
 	 */
-	int CURRENT_PROTOCOL = 20;
+	int CURRENT_PROTOCOL = 60;
 
 
-	byte LOGIN_PACKET = (byte) 0x82;
-	byte LOGIN_STATUS_PACKET = (byte) 0x83;
-
-	byte MESSAGE_PACKET = (byte) 0x85;
-	byte SET_TIME_PACKET = (byte) 0x86;
-	byte START_GAME_PACKET = (byte) 0x87;
-	byte ADD_MOB_PACKET = (byte) 0x88;
-	byte ADD_PLAYER_PACKET = (byte) 0x89;
-	byte REMOVE_PLAYER_PACKET = (byte) 0x8a;
-
-	byte ADD_ENTITY_PACKET = (byte) 0x8c;
-	byte REMOVE_ENTITY_PACKET = (byte) 0x8d;
-	byte ADD_ITEM_ENTITY_PACKET = (byte) 0x8e;
-	byte TAKE_ITEM_ENTITY_PACKET = (byte) 0x8f;
-	byte MOVE_ENTITY_PACKET = (byte) 0x90;
-
-	byte ROTATE_HEAD_PACKET = (byte) 0x94;
-	byte MOVE_PLAYER_PACKET = (byte) 0x95;
-	//const PLACE_BLOCK_PACKET = (byte) 0x96;
-	byte REMOVE_BLOCK_PACKET = (byte) 0x97;
-	byte UPDATE_BLOCK_PACKET = (byte) 0x98;
-	byte ADD_PAINTING_PACKET = (byte) 0x99;
-	byte EXPLODE_PACKET = (byte) 0x9a;
-	byte LEVEL_EVENT_PACKET = (byte) 0x9b;
-	byte TILE_EVENT_PACKET = (byte) 0x9c;
-	byte ENTITY_EVENT_PACKET = (byte) 0x9d;
-
-	byte PLAYER_EQUIPMENT_PACKET = (byte) 0xa0;
-	byte PLAYER_ARMOR_EQUIPMENT_PACKET = (byte) 0xa1;
-	byte INTERACT_PACKET = (byte) 0xa2;
-	byte USE_ITEM_PACKET = (byte) 0xa3;
-	byte PLAYER_ACTION_PACKET = (byte) 0xa4;
-
-	byte HURT_ARMOR_PACKET = (byte) 0xa6;
-	byte SET_ENTITY_DATA_PACKET = (byte) 0xa7;
-	byte SET_ENTITY_MOTION_PACKET = (byte) 0xa8;
-	//byte SET_ENTITY_LINK_PACKET = (byte) 0xa9;
-	byte SET_HEALTH_PACKET = (byte) 0xaa;
-	byte SET_SPAWN_POSITION_PACKET = (byte) 0xab;
-	byte ANIMATE_PACKET = (byte) 0xac;
-	byte RESPAWN_PACKET = (byte) 0xad;
-	byte SEND_INVENTORY_PACKET = (byte) 0xae;
-	byte DROP_ITEM_PACKET = (byte) 0xaf;
-	byte CONTAINER_OPEN_PACKET = (byte) 0xb0;
-	byte CONTAINER_CLOSE_PACKET = (byte) 0xb1;
-	byte CONTAINER_SET_SLOT_PACKET = (byte) 0xb2;
-	byte CONTAINER_SET_DATA_PACKET = (byte) 0xb3;
-	byte CONTAINER_SET_CONTENT_PACKET = (byte) 0xb4;
-	//byte CONTAINER_ACK_PACKET = (byte) 0xb5;
-	byte CHAT_PACKET = (byte) 0xb6;
-	byte ADVENTURE_SETTINGS_PACKET = (byte) 0xb7;
-	byte ENTITY_DATA_PACKET = (byte) 0xb8;
-	//byte PLAYER_INPUT_PACKET = (byte) 0xb9;
-	byte FULL_CHUNK_DATA_PACKET = (byte) 0xba;
-	byte UNLOAD_CHUNK_PACKET = (byte) 0xbb;
+bbconst ATTRIBUTE_PACKET = 0x60;
+	const LOGIN_PACKET = 0x8f;
+	const PLAY_STATUS_PACKET = 0x90;
+	
+	const DISCONNECT_PACKET = 0x91;
+	const BATCH_PACKET = 0x92;
+	const TEXT_PACKET = 0x93;
+	const SET_TIME_PACKET = 0x94;
+	const START_GAME_PACKET = 0x95;
+	const ADD_PLAYER_PACKET = 0x96;
+	const REMOVE_PLAYER_PACKET = 0x97;
+	const ADD_ENTITY_PACKET = 0x98;
+	const REMOVE_ENTITY_PACKET = 0x99;
+	const ADD_ITEM_ENTITY_PACKET = 0x9a;
+	const TAKE_ITEM_ENTITY_PACKET = 0x9b;
+	const MOVE_ENTITY_PACKET = 0x9c;
+	const MOVE_PLAYER_PACKET = 0x9d;
+	const REMOVE_BLOCK_PACKET = 0x9e;
+	const UPDATE_BLOCK_PACKET = 0x9f;
+	const ADD_PAINTING_PACKET = 0xa0;
+	const EXPLODE_PACKET = 0xa1;
+	const LEVEL_EVENT_PACKET = 0xa2;
+	const BLOCK_EVENT_PACKET = 0xa3;
+	const ENTITY_EVENT_PACKET = 0xa4;
+	const MOB_EFFECT_PACKET = 0xa5;
+	const UPDATE_ATTRIBUTES_PACKET = 0xa6;
+	const MOB_EQUIPMENT_PACKET = 0xa7;
+	const MOB_ARMOR_EQUIPMENT_PACKET = 0xa8;
+	const INTERACT_PACKET = 0xa9;
+	const USE_ITEM_PACKET = 0xaa;
+	const PLAYER_ACTION_PACKET = 0xab;
+	const HURT_ARMOR_PACKET = 0xac;
+	const SET_ENTITY_DATA_PACKET = 0xad;
+	const SET_ENTITY_MOTION_PACKET = 0xae;
+	const SET_ENTITY_LINK_PACKET = 0xaf;
+	const SET_HEALTH_PACKET = 0xb0;
+	const SET_SPAWN_POSITION_PACKET = 0xb1;
+	const ANIMATE_PACKET = 0xb2;
+	const RESPAWN_PACKET = 0xb3;
+	const DROP_ITEM_PACKET = 0xb4;
+	const CONTAINER_OPEN_PACKET = 0xb5;
+	const CONTAINER_CLOSE_PACKET = 0xb6;
+	const CONTAINER_SET_SLOT_PACKET = 0xb7;
+	const CONTAINER_SET_DATA_PACKET = 0xb8;
+	const CONTAINER_SET_CONTENT_PACKET = 0xb9;
+	const CRAFTING_DATA_PACKET = 0xba;
+	const CRAFTING_EVENT_PACKET = 0xbb;
+	const ADVENTURE_SETTINGS_PACKET = 0xbc;
+	const BLOCK_ENTITY_DATA_PACKET = 0xbd;
+	const PLAYER_INPUT_PACKET = 0xbe;
+	const FULL_CHUNK_DATA_PACKET = 0xbf;
+	const SET_DIFFICULTY_PACKET = 0xc0;
+	const CHANGE_DIMENSION_PACKET = 0xc1;
+	const SET_PLAYER_GAMETYPE_PACKET = 0xc2;
+	const PLAYER_LIST_PACKET = 0xc3;
+	const TELEMETRY_EVENT_PACKET = 0xc4;
+    const CLIENTBOUND_MAP_ITEM_DATA_PACKET = 0xc6;
+    const MAP_INFO_REQUEST_PACKET = 0xc7;
+    const REQUEST_CHUNK_RADIUS_PACKET = 0xc8;
+    const CHUNK_RADIUS_UPDATE_PACKET = 0xc9;
+    const ITEM_FRAME_DROP_ITEM_PACKET = 0xca;
 
 }
